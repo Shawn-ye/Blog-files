9024a0e90c9aa29a4d25e32f828eab9e05c8ca52
SHUANG.YE <shuang.ye@ecovacs.com>
2018-03-26 13:36:35 +0800

    add template

---
+++ algorithms/sort_practice.js
@@ -0,0 +1,23 @@
+let tools = require("./lib");
+
+function bubleSort(arr)    {
+    
+}
+
+function insertSort(arr)    {
+
+}
+
+function selectSort(arr)    {
+
+}
+
+function mergedSort(arr)    {
+
+}
+
+function quickSort(arr) {
+
+}
+
+
