0310aa04e9bd39bbf383979f7b3c821fc0f2cd1f
SHUANG.YE <shuang.ye@ecovacs.com>
2018-03-26 13:41:45 +0800

    update template

--- algorithms/sort_practice.js
+++ algorithms/sort_practice.js
@@ -1,23 +1,34 @@
 let tools = require("./lib");
 
+function swap (arr, x, y)   {
+    let temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+}
+
 function bubleSort(arr)    {
-    
+    return arr;
 }
 
 function insertSort(arr)    {
-
+    return arr;
 }
 
 function selectSort(arr)    {
-
-}
+    return arr;
+}   
 
 function mergedSort(arr)    {
-
+    return arr;
 }
 
 function quickSort(arr) {
-
+    return arr;
 }
 
+console.log("buble sort", bubleSort(tools.genRandomArr()));
+console.log("insertSort", insertSort(tools.genRandomArr()));
+console.log("selectSort", selectSort(tools.genRandomArr()));
+console.log("mergedSort", mergedSort(tools.genRandomArr()));
+console.log("quickSort", quickSort(tools.genRandomArr()));
 
