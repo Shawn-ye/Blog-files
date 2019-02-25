ef4afbe113372786c01c05c5870a324062eb3e70
Shawn <shawnye90@163.com>
2019-02-26 07:37:58 +0800

    Update start.sh

--- dockerfile_build/ss-rasp/start.sh
+++ dockerfile_build/ss-rasp/start.sh
@@ -2,27 +2,27 @@
 curl https://proxyservices.azurewebsites.net/api/RandomPwd?code=$APPKEY > temp
 temp2=`cat temp`
 
-sslocal -s $SERVER -p 443 -b 0.0.0.0 -l 1234 -k $temp2 &
+sslocal -s ${SERVER} -p 443 -b 0.0.0.0 -l 1234 -k ${temp2} &
 pwdDate=`TZ=UTC-5 date +%m%d`
 
 while true
 do
 	currentDate=`TZ=UTC-5 date +%m%d`
-	if [ $currentDate != $pwdDate ]
+	if [ ${currentDate} != ${pwdDate} ]
 	then
-		curl https://proxyservices.azurewebsites.net/api/RandomPwd?code=$APPKEY > temp
+		curl https://proxyservices.azurewebsites.net/api/RandomPwd?code=${APPKEY} > temp
 		returncode=`echo $?`
-		if [ $returncode == 0 ]
+		if [ ${returncode} == 0 ]
 		then
 			temp2=`cat temp`
 			pkill -f sslocal
-			sslocal -s $SERVER -p 443 -b 0.0.0.0 -l 1234 -k $temp2 &
+			sslocal -s ${SERVER} -p 443 -b 0.0.0.0 -l 1234 -k ${temp2} &
 			pwdDate=`TZ=UTC-5 date +%m%d`
-			echo "Password updated for $pwdDate"
+			echo "Password updated for ${pwdDate}"
 		else
 			echo "Update password failed, wait for retry"
 		fi
 	fi
-	echo "$currentDate $pwdDate $minute"
+	echo "${currentDate} ${pwdDate} ${minute}"
 	sleep 20
 done
