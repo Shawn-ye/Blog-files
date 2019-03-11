e8f1e4c59de37415817f69396c7bfd6b8215f672
Shawn <shawnye90@163.com>
2019-03-11 18:06:30 +0800

    update

--- dockerfile_build/server_restart/stat.sh
+++ dockerfile_build/server_restart/stat.sh
@@ -7,7 +7,7 @@ containerid=`cat /etc/hostname`
 while true
 do
     txbytes=`cat /sys/class/net/eth0/statistics/tx_bytes`
-    curl 'https://proxyservices.azurewebsites.net/api/TrafficStat?code=$APPKEY&containerid=$containerid&bytes=$txbytes&port=$PORT'
+    curl 'https://proxyservices.azurewebsites.net/api/TrafficStat?code=$APPKEY&containerid=$containerid&host=$HOST&bytes=$txbytes&port=$PORT'
     sleep 300
 done
 
